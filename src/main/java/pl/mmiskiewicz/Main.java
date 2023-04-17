b607b8b1f7327388a1aa4259affbeb43a454250b
MichalMiskiewicz <miskiewiczmd@gmail.com>
2023-04-17 14:06:01 +0200

    add endpoint which returns all customers

--- src/main/java/pl/mmiskiewicz/Main.java
+++ src/main/java/pl/mmiskiewicz/Main.java
@@ -3,6 +3,7 @@ package pl.mmiskiewicz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,10 +13,22 @@ import java.util.List;
 @EnableAutoConfiguration*/
 @SpringBootApplication
 @RestController
+@RequestMapping("/api/v1/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
     }
 
     /*@GetMapping("/greet")
