70731c5dcfbf87792cd730d8f06e2dcb7049e5cc
MichalMiskiewicz <miskiewiczmd@gmail.com>
2023-04-17 12:39:38 +0200

    Example of getting json response after calling an endpoint

---
+++ src/main/java/pl/mmiskiewicz/Main.java
@@ -0,0 +1,38 @@
+package pl.mmiskiewicz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+/*@ComponentScan(basePackages = "pl.mmiskiewicz")
+@EnableAutoConfiguration*/
+@SpringBootApplication
+@RestController
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/greet")
+    public GreetResponse greet() {
+        GreetResponse response = new GreetResponse(
+                "Hello",
+                List.of("Java", "Golang", "JavaScript"),
+                new Person("Alex", 24, 30_000)
+        );
+
+        return response;
+    }
+
+    record Person (String name, int age, double savings){}
+    record GreetResponse(
+            String greet,
+            List<String> favPogrammingLanguages,
+            Person person
+    ) {}
+}
